e4ba178f04f089ace1bbc4e0999215a91dcb2dae
oulinguo <oulinguo@fongwell.com>
2017-12-04 13:44:47 +0800

    Signed-off-by: oulinguo <oulinguo@fongwell.com>

--- client/views/compatible/compatible.jsx
+++ client/views/compatible/compatible.jsx
@@ -11,6 +11,7 @@ class Function extends React.Component {
                             <li><a href="#"><img src={require("img/logo.png")} alt="" /></a></li>
                             <li className="item"><a href="#">产品与服务</a></li>
                             <li className="item"><a href="#">解决方案</a></li>
+                            <li className="item"><a href="#">动态新闻</a></li>
                             <li className="item"><a href="#">关于我们</a></li></ul>
                         <ul className="topBoxright flex-vcenter">
                             <li><a href="#">注册</a></li><li><a href="#">登陆</a></li><li className="blueBtn"><a href="#">测试者社区</a></li>
